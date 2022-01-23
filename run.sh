ff2b57c72fd97c1e9ad68773b4c4cf4bf96394a8
Kaijie Fu <u1314550@utah.edu>
2022-01-23 10:29:15 -0700

    UPDATE: updated commands for init swagger2

--- run.sh
+++ run.sh
@@ -2,4 +2,8 @@ go mod tidy
 cd ./model/migrate_scripts || exit
 go run migrate_scripts.go || echo >> "migrate failed"
 cd ../../ || exit
+
+go get -u github.com/swaggo/swag/cmd/swag
+swag init --parseDependency --parseInternal
+
 go run main.go || echo >> "start server failed"

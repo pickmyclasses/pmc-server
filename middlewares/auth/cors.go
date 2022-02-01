a61dc1bc59219644af0dcb1469e1dedd2eac5ba0
Kaijie Fu <u1314550@utah.edu>
2022-02-01 13:19:40 -0700

    FIX: fixed post bug in cors middleware

--- middlewares/auth/cors.go
+++ middlewares/auth/cors.go
@@ -1,25 +1,34 @@
 package auth
 
 import (
-	"net/http"
-
-	"go.uber.org/zap"
-
 	"github.com/gin-gonic/gin"
 )
 
 func Cors() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		zap.L().Info("cors used")
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
-		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
-		c.Header("Access-Control-Allow-Credentials", "true")
+	//return func(c *gin.Context) {
+	//	method := c.Request.Method
+	//	c.Header("Access-Control-Allow-Origin", "*")
+	//	c.Header("Access-Control-Allow-Headers", "Content-Type,AccessToken,X-CSRF-Token, Authorization, Token, x-token")
+	//	c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PATCH, PUT")
+	//	c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Content-Type")
+	//	c.Header("Access-Control-Allow-Credentials", "true")
+	//
+	//	if method == "OPTIONS" {
+	//		c.AbortWithStatus(http.StatusNoContent)
+	//	}
+	//}
 
-		if method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
+	return func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
 		}
+
+		c.Next()
 	}
 }

dcda5ca9dc664cf522829ec8c3e029b014bf9228
Leon Tran <49257836+leontmd@users.noreply.github.com>
2022-04-10 11:04:05 -0600

    added DELETE college api

--- schedule-feedback-api/functions.js
+++ schedule-feedback-api/functions.js
@@ -227,6 +227,19 @@ const deleteClass = (request, response) =>
   })
 }
 
+const deleteCollege = (request, response) =>
+{
+  const id = parseInt(request.params.id)
+
+  pool.query('DELETE FROM public.college WHERE id = $1', [id], (error, results) => {
+    if (error) 
+    {
+      response.status(400).json(error);
+    }
+    response.status(201).send({"message" : "Delete data successfully"});
+  })
+}
+
 // ----------------------------------------------- Class functions -------------------------------------------------------------------------------
 const updateClass = (request, response) => {
   const { id, created_at, deleted_at, is_deleted, semester, year, session, wait_list, offer_date, start_time, end_time, location, recommendation_score, 
@@ -248,18 +261,7 @@ const updateClass = (request, response) => {
 
 
 // ----------------------------------------------- College functions -------------------------------------------------------------------------------
-const deleteCollege = (request, response) =>
-{
-  const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM public.college WHERE id = $1', [id], (error, results) => {
-    if (error) 
-    {
-      response.status(400).json(error);
-    }
-    response.status(201).send({"message" : "Delete data successfully"});
-  })
-}
 
 const updateCollege = (request, response) => {
   const { id, created_at, deleted_at, is_deleted, name } = request.body
